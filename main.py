36c1671ed890472baf3f9dd10b975aa2cdd8a44c
4ntim4tter <4ntim4tter@gmail.com>
2022-05-27 22:37:56 +0200

    created database class for global variables

--- main.py
+++ main.py
@@ -6,21 +6,45 @@ from tkinter import ttk
 from tkinter import messagebox
 import tkinter.font as tkFont
 
-#stored data from files
-DUPLICATE_CHECK_DATA = []
-NAME_AUTO_COMPLETE = []
-REG_BROJ_COMPLETE = []
-CUSTOMER_DATA = []
+#data class for parsing files to cache
+class DataVariables(object):
+    def __init__(self):
+        self.duplicate_data_check = []
+        self.name_auto_complete = []
+        self.reg_auto_complete = []
+        self.customer_data = []
+        
+    def parse_data(self):
+        with open('data.csv', 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                self.customer_data.append(row)
+                
+        with open('data.csv', 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for data in reader:
+                self.name_auto_complete.append(data['ime'].title())
+                    
+        with open('data.csv', 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for data in reader:
+                if data['reg_broj'] in self.reg_auto_complete:
+                    pass
+                else:
+                    self.reg_auto_complete.append(data['reg_broj'].upper())
+    
+        #data for checking duplicates
+        for data in self.customer_data:
+            self.duplicate_data_check.append('{},{},{}'.format(data['ime'].lower(),data['vozilo'].lower(),data['reg_broj'].lower()))
+        
 
-#check if data file exists, create new file if not
+#check if data directory exists, create new if not
 JOBS_STORAGE_PATH = os.path.join(os.path.dirname(__file__) + '/jobs/')
 if not os.path.isdir(JOBS_STORAGE_PATH):
     os.mkdir(JOBS_STORAGE_PATH)
 
-
-if os.path.isfile('data.csv'):
-    pass
-else:
+#create data.csv if one does not exist in root
+if not os.path.isfile('data.csv'):
     with open('data.csv', 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         writer.writerow(['ime', 'vozilo', 'reg_broj'])
@@ -33,11 +57,11 @@ def create_new_customer(name:str, car:str, reg_broj:str):
 
 
 #FIND CUSTOMER ADD NEW WORK TO EXISTING WORK FILE
-def find_customer_window(master_window, CUSTOMER_DATA, NAME_AUTO_COMPLETE, REG_BROJ_COMPLETE, font_style):
+def find_customer_window(master_window, database, font_style):
       
     def get_name(event):
         if name_combo.focus_get():
-            customer = next(item for item in CUSTOMER_DATA if item['ime'] == name_combo.get().title())
+            customer = next(item for item in database.customer_data if item['ime'] == name_combo.get().title())
             car_combo.set(customer['vozilo'])
             reg_combo.set(customer['reg_broj'])
             
@@ -51,7 +75,7 @@ def find_customer_window(master_window, CUSTOMER_DATA, NAME_AUTO_COMPLETE, REG_B
             
     def get_reg_broj(event):
         if reg_combo.focus_get():
-            customer = next(item for item in CUSTOMER_DATA if item['reg_broj'] == reg_combo.get().upper())
+            customer = next(item for item in database.customer_data if item['reg_broj'] == reg_combo.get().upper())
             name_combo.set(customer['ime'])
             car_combo.set(customer['vozilo'])
             
@@ -64,12 +88,11 @@ def find_customer_window(master_window, CUSTOMER_DATA, NAME_AUTO_COMPLETE, REG_B
                     
     
     frame = tk.Frame(master_window)
-    #frame.title('Pretraga Mušterije')
-    frame.grid()
+    frame.pack(side='top', padx=5, pady=5, fill='both')
      
     #Ime i prezime label and combobox
     ttk.Label(frame, font=font_style, text='Ime i Prezime').pack(side='top')
-    name_combo = autocomplete.AutocompleteCombobox(frame, width=20, completevalues=NAME_AUTO_COMPLETE, font=font_style)
+    name_combo = autocomplete.AutocompleteCombobox(frame, width=20, completevalues=database.name_auto_complete, font=font_style)
     name_combo.pack(side='top')
     name_combo.bind('<Return>', get_name)
     
@@ -80,7 +103,7 @@ def find_customer_window(master_window, CUSTOMER_DATA, NAME_AUTO_COMPLETE, REG_B
     
     #Registracija label and combobox
     ttk.Label(frame, font=font_style, text='Reg. Broj').pack(side='top')
-    reg_combo = autocomplete.AutocompleteCombobox(frame, width=20, completevalues=REG_BROJ_COMPLETE, font=font_style)
+    reg_combo = autocomplete.AutocompleteCombobox(frame, width=20, completevalues=database.reg_auto_complete, font=font_style)
     reg_combo.pack(side='top')
     reg_combo.bind('<Return>', get_reg_broj)
     
@@ -115,26 +138,26 @@ def find_customer_window(master_window, CUSTOMER_DATA, NAME_AUTO_COMPLETE, REG_B
 
 
 #ADD CUSTOMER AND CREATE WORK DATA FILE
-def add_customer_window(master_window, font_style):
+def add_customer_window(master_window, database, font_style):
     
     def add_to_csv():
         if name_add_text.get() == '' or car_add_text.get() == '' or reg_add_text.get() == '':
             messagebox.showwarning('Upozorenje!', 'Jedno ili više polja je prazno!')
             return None
         
-        if f'{name_add_text.get().lower()},{car_add_text.get().lower()},{reg_add_text.get().lower()}' in DUPLICATE_CHECK_DATA:
+        if f'{name_add_text.get().lower()},{car_add_text.get().lower()},{reg_add_text.get().lower()}' in database.duplicate_data_check:
             messagebox.showwarning('Upozorenje!', 'Korisnik i Registracija već postoje u bazi podataka.')
             return None
 
         #update cache variables and data for new user real time
         create_new_customer(name_add_text.get(),car_add_text.get(), reg_add_text.get())
-        messagebox.showinfo('Uspjeh', 'Nova mušterija dodana u bazu podataka.')
-        DUPLICATE_CHECK_DATA.append(f'{name_add_text.get().lower()},{car_add_text.get().lower()},{reg_add_text.get().lower()}')
-        CUSTOMER_DATA.append({'ime':name_add_text.get().title(), 
+        database.duplicate_data_check.append(f'{name_add_text.get().lower()},{car_add_text.get().lower()},{reg_add_text.get().lower()}')
+        database.customer_data.append({'ime':name_add_text.get().title(), 
                               'vozilo':car_add_text.get().title(), 
                               'reg_broj':reg_add_text.get().upper()})
-        NAME_AUTO_COMPLETE.append(name_add_text.get().title())
-        REG_BROJ_COMPLETE.append(reg_add_text.get().upper())
+        database.name_auto_complete.append(name_add_text.get().title())
+        database.reg_auto_complete.append(reg_add_text.get().upper())
+        messagebox.showinfo('Uspjeh', 'Nova mušterija dodana u bazu podataka.')
         
         name_reg_nospace = name_add_text.get().lower().replace(' ', '') + '_' + reg_add_text.get().upper().replace(' ', '')
         
@@ -145,7 +168,7 @@ def add_customer_window(master_window, font_style):
          
     
     frame = tk.Frame(master_window)
-    frame.grid()
+    frame.pack(side='top', padx=5, pady=5, fill='both')
     
     #Add new customer name
     ttk.Label(frame, font=font_style, text='Ime i Prezime').pack(side='top')
@@ -162,37 +185,17 @@ def add_customer_window(master_window, font_style):
     reg_add_text = tk.Entry(frame, width=20, font=font_style)
     reg_add_text.pack(side='top')
     
-    confirm_button = ttk.Button(frame, text='Dodaj Mušteriju', style='bttn_style.TButton', command=add_to_csv)
+    confirm_button = ttk.Button(frame, text='Unos u Bazu', style='bttn_style.TButton', command=add_to_csv)
     confirm_button.pack(side='top')
 
 
 def main():
     #store data from csv in cache for use
-    with open('data.csv', 'r', encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            CUSTOMER_DATA.append(row)
-             
-    with open('data.csv', 'r', encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-        for data in reader:
-            NAME_AUTO_COMPLETE.append(data['ime'].title())
-                
-    with open('data.csv', 'r', encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-        for data in reader:
-            if data['reg_broj'] in REG_BROJ_COMPLETE:
-                pass
-            else:
-                REG_BROJ_COMPLETE.append(data['reg_broj'].upper())
-    
-    #data for checking duplicates
-    for data in CUSTOMER_DATA:
-        DUPLICATE_CHECK_DATA.append('{},{},{}'.format(data['ime'].lower(),data['vozilo'].lower(),data['reg_broj'].lower()))
-    
+    database = DataVariables()
+    database.parse_data()
     
     def clear_children():
-        list_of_slaves = master_window.grid_slaves()
+        list_of_slaves = master_window.pack_slaves()
         for frame in list_of_slaves:
             if '!frame' in str(frame):
                 frame.destroy()
@@ -212,13 +215,13 @@ def main():
     
     #search for customers button
     query_customer_button = ttk.Button(master_window, width=20,text='Pretraga Mušterije', style='bttn_style.TButton', 
-                                       command=lambda:[clear_children(), find_customer_window(master_window, CUSTOMER_DATA, NAME_AUTO_COMPLETE, REG_BROJ_COMPLETE, font_style)])
-    query_customer_button.grid(row=0, column=0)
+                                       command=lambda:[clear_children(), find_customer_window(master_window, database, font_style)])
+    query_customer_button.pack(side='top', anchor="nw", fill='x', padx=2, pady=(0, 5))
     
     #add new customer button
     add_customer_button = ttk.Button(master_window, width=20, text='Dodaj Mušteriju', style='bttn_style.TButton', 
-                                     command=lambda:[clear_children(), add_customer_window(master_window, font_style)])
-    add_customer_button.grid(row=1, column=0)
+                                     command=lambda:[clear_children(), add_customer_window(master_window, database, font_style)])
+    add_customer_button.pack(side='top', anchor="nw", fill='x', padx=2, pady=(0, 0))
     
     master_window.mainloop()
 
